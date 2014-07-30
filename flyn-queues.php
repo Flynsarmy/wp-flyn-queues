87940651f6142456007fe1a7d45974512321d898
flynsarmy <flynsarmy@gmail.com>
2014-07-30 11:58:30 +1000

    first commit

---
+++ flyn-queues.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package Flynsarmy Queues
+ * @version 1.0.0
+ *
+ * Plugin Name: Flynsarmy Queues
+ * Description: Adds task queue functionality to WordPress
+ * Author: Flynsarmy
+ * Version: 0.1
+ * Author URI: http://www.flynsarmy.com
+ */
+
+require __DIR__.'/vendor/autoload.php';
+
+add_action('wp_ajax_flyn_queues_run_wp_cron', 'flyn_queues_run_wp_cron');
+add_action('wp_ajax_nopriv_flyn_queues_run_wp_cron', 'flyn_queues_run_wp_cron');
+function flyn_queues_run_wp_cron()
+{
+	set_time_limit(0);
+	global $wpdb;
+
+	$blogs = $wpdb->get_results("SELECT domain, path FROM $wpdb->blogs WHERE archived=0 AND deleted=0");
+
+	foreach($blogs as $blog) {
+		$url = "http://" . $blog->domain . ($blog->path ? $blog->path : '/') . 'wp-cron.php?doing_wp_cron';
+		echo $url . "<br/>\n";
+		flush();
+
+		wp_remote_get($url, [
+			'timeout' => 0,
+			'blocking' => false,
+		]);
+	}
+	exit;
+}
+
+/**
+ * Create a new queue item.
+ *
+ * Example Usage:
+ * flyn_queues_push(function(array $args) {
+ *     $fp = fopen(ABSPATH.'log.log', 'a');
+ *     fprintf($fp, "timeAtCreate: %s, time now: %s\n", $args['timeAtCreate'], time());
+ *     fclose($fp);
+ * }, ['timeAtCreate' => time()]);
+ *
+ * @param  callable $callback   Callback to execute when the scheduled event is
+ *                              triggered.
+ *                              e.g function(array $args) {}
+ * @param  array    $args       Array of argumens to pass to the callback
+ * @param  int      $timestamp  Timestamp to trigger the task. Defaults to 'as soon as possible'.
+ *
+ * @return (bool|mull)          False if the event was cancelled by a plugin, null otherwise.
+ */
+function flyn_queues_push(callable $callback, array $args = [], $timestamp = 0)
+{
+	$callback = new Jeremeamia\SuperClosure\SerializableClosure($callback);
+
+	// Scheduling an event to occur before 10 minutes after an existing event
+	// of the same name will be ignored, unless you pass unique values for
+	// $args. So do that.
+	$unique_value = time()+uniqid();
+
+	return wp_schedule_single_event( $timestamp ?: time(), 'flyn_queues_trigger', [
+		serialize($callback), $args, $unique_value
+	]);
+}
+
+add_action('flyn_queues_trigger', function($serializedCallback, array $args = []) {
+	$callback = unserialize($serializedCallback);
+	$callback($args);
+}, 10, 2);
---
+++ readme.md
@@ -0,0 +1,49 @@
+# Flyn Queues (Multisite supported)
+
+Flyn Queues is a WordPress plugin that adds task queue functionality to WordPress.
+
+## Requirements
+
+* Composer
+* PHP 5.4+
+
+## Installation
+
+* Run `git clone https://github.com/Flynsarmy/wp-flyn-queues.git /path/to/wp-content/plugins/flyn-queues`
+* `composer update` in the new plugin directory
+* Network Activate the plugin
+
+
+### Optional (Recommended) - Replace wp-cron with cron
+
+This plugin uses the built in `wp_schedule_single_event` function, so disabling WP cron and calling it manually is recommended.
+
+* In *wp-config.php* add `define('DISABLE_WP_CRON', 'true');`
+* If on multisite, in your servers crontab (or cpanel) add a minutely cron for `wget http://yoursite.com/wp-admin/admin-ajax.php?action=flyn_queues_run_wp_cron >/dev/null 2>&1`
+* If not on multisite, in your servers crontab (or cpanel) add a minutely cron for `wget http://yoursite.com/wp-cron.php?doing_wp_cron >/dev/null 2>&1`
+
+## Usage
+
+To push a task to the queue:
+
+> (bool|null) flyn\_queues\_push(callable $callback, [array $args = array(), [int $timestamp = 0]])
+
+Accepts:
+|-------------------------------------------------------------------------------------------|
+| Type     | Argument   | Description                                                       |
+|-------------------------------------------------------------------------------------------|
+| callable | $callback  | Callback to be executed. Accepts an array $args parameter.        |
+| array    | $args      | (optional) Array of arguments to pass to $callback.               |
+| int      | $timestamp | (optional) Earliest time to be executed. Defaults to immediately. |
+|-------------------------------------------------------------------------------------------|
+
+### Examples
+
+1. Creates a *log.log* file in WP root folder containing the time the task was created and the time it was run:
+```php
+flyn_queues_push(function(array $args) {
+	$fp = fopen(ABSPATH.'log.log', 'a');
+	fprintf($fp, "timeAtCreate: %s, time now: %s\n", $args['timeAtCreate'], time());
+	fclose($fp);
+}, ['timeAtCreate' => time()]);
+```
